fe17a57e49c686de0489f74841a8db15f0a9d695
Dat Duong <datd1988@gmail.com>
2021-09-17 19:03:56 -0400

    Update GetFinalCorrR2.R

--- GetFinalCorrR2.R
+++ GetFinalCorrR2.R
@@ -3,16 +3,18 @@ library('ggplot2')
 
 # ---------------------------------------------------------------------------------------------------------
 
-mods = c('b4ns448w1ss10lr1e-05dp0.2b32ntest1-Img+6F+SoftM0.9T0.8', 'b4ns448w1ss10lr1e-05dp0.2b32ntest1-Img+6F+Soft', 'b4ns448w1ss10lr1e-05dp0.2b32ntest1-Img+Meta2+6F' , 'b4ns448w1ss10lr1e-05dp0.2b32ntest1-Img+Meta2+logMAR+6F')
+# ! get correlation and regression R2 on neural network models that predict FH scores or Driving score. 
 
-for (model in mods) {
+mods = c('b4ns448w1ss10lr1e-05dp0.2b32ntest1-Img+6F') # ! put your model name here. 
+
+for (model in mods) { # ! go over each model name
   print (model)
   cor_ = c() 
   r2 = c() 
 
-  df_ = read.csv(paste0('C:/Users/duongdb/Documents/FH_OCT_08172021/Classify/',model,'/final_prediction.csv'))
+  df_ = read.csv(paste0('C:/Users/duongdb/Documents/FH_OCT_08172021/Classify/',model,'/final_prediction.csv')) # ! read in final prediction (change csv name if needed.)
   cor_ = c(cor_,cor(df_$label, df_$average_score, method = c("spearman"))) # pearson spearman
-  lmmod = lm(df_$label ~ df_$average_score)
+  lmmod = lm(df_$label ~ df_$average_score) # linear regression R2
   r2 = c(r2, summary(lmmod)$r.squared)
   
   print (summary(cor_))
@@ -21,40 +23,20 @@ for (model in mods) {
 }
 
 
-# ---------------------------------------------------------------------------------------------------------
-
-# ! decision tree... same code as neural net can be used here. 
-mods = c('DecisionTreeDriving', 'DecisionTreeDrivingMeta2', 'DecisionTreeDrivingMeta2+logMAR', 'DecisionTreeDrivingMeta2+logMAR+FH')
-
-for (model in mods) {
-  print (model)
-  cor_ = c() 
-  r2 = c() 
-
-  df_ = read.csv(paste0('C:/Users/duongdb/Documents/FH_OCT_08172021/',model,'/final_prediction.csv'))
-  cor_ = c(cor_,cor(df_$label, df_$average_score, method = c("spearman"))) # pearson spearman
-  lmmod = lm(df_$label ~ df_$average_score)
-  r2 = c(r2, summary(lmmod)$r.squared)
-  
-  print (summary(cor_))
-  print (summary(r2))
-  print (table(df_$label))
-}
 
 # ---------------------------------------------------------------------------------------------------------
 
+# ! get correlation and regression R2 on neural network models that predict logMAR. 
 
-# ! predict logMAR
+mods = c('b4ns448w1ss5lr0.0005dp0.2b32ntest1-Img+6F+logMAR') # ! put your model name here. 
 
-mods = c('b4ns448w1ss5lr0.0005dp0.2b32ntest1-Img+6F+logMAR', 'b4ns448w1ss5lr0.0005dp0.2b32ntest1-Img+Meta+6F+logMAR', 'b4ns448w1ss5lr0.0005dp0.2b32ntest1-Img+Meta2+6F+logMAR', 'b4ns448w1ss5lr0.0005dp0.2b32ntest1-Img+Meta2+FHScore+6F+logMAR')
-
-for (model in mods) {
+for (model in mods) { # ! go over each model name
   print (model)
   cor_ = c() 
   r2 = c() 
   average_score = NULL 
-  for (fold in c(0,1,2,3,4)){
-    df_ = read.csv(paste0('C:/Users/duongdb/Documents/FH_OCT_08172021/Classify/',model,'/test_on_fold_0_from_fold',fold,'.csv'))
+  for (fold in c(0,1,2,3,4)){ # ! go over each fold.
+    df_ = read.csv(paste0('C:/Users/duongdb/Documents/FH_OCT_08172021/Classify/',model,'/test_on_fold_0_from_fold',fold,'.csv')) # ! take average logMAR over 5 folds
     if (is.null(average_score)){ 
       average_score = unlist(df_['X0']) 
     } else { 
@@ -63,7 +45,7 @@ for (model in mods) {
   }
   average_score = average_score / 5
   df_$average_score = average_score
-  cor_ = c(cor_,cor(df_$logMAR, df_$average_score, method = c("spearman"))) # pearson spearman
+  cor_ = c(cor_,cor(df_$logMAR, df_$average_score, method = c("spearman"))) # ! call @logMAR instead of @label 
   lmmod = lm(df_$logMAR ~ df_$average_score)
   r2 = c(r2, summary(lmmod)$r.squared)
   print (summary(cor_))
@@ -74,18 +56,17 @@ for (model in mods) {
 
 # ---------------------------------------------------------------------------------------------------------
 
-# ! regression predict logMAR 
+# ! get correlation and regression R2 on ELASTIC NET ( NOT NEURAL NETWORK ) models that predict logMAR. 
 
-mods = c('ElasticNetLogMAR','ElasticNetLogMARMeta2','ElasticNetLogMARMeta2+FH','ElasticNetLogMAR+FH')
+mods = c('ElasticNetLogMAR') # ! put your model name here. 
 
-# mods = c('ElasticNetLogMAR+FH')
-for (model in mods) {
+for (model in mods) { # ! go over each model name
   print (model)
   cor_ = c() 
   r2 = c() 
   average_score = NULL 
   for (fold in c(0,1,2,3,4)){
-    df_ = read.csv(paste0('C:/Users/duongdb/Documents/FH_OCT_08172021/',model,'/test_on_fold_5_from_fold',fold,'.csv'))
+    df_ = read.csv(paste0('C:/Users/duongdb/Documents/FH_OCT_08172021/',model,'/test_on_fold_5_from_fold',fold,'.csv')) # ! take average logMAR over 5 folds
     if (is.null(average_score)){ 
       average_score = unlist(df_['average_score']) 
     } else { 
@@ -102,23 +83,3 @@ for (model in mods) {
   # print (summary(lmmod))
 }
 
-
-# ---------------------------------------------------------------------------------------------------------
-
-mods = c('b4ns448w2-4ss10lr1e-05dp0.2b32ntest1-Img+Meta2+6F+driving','b4ns448w2-4ss10lr1e-05dp0.2b32ntest1-Img+Meta2+logMAR+6F+driving','b4ns448w2-4ss10lr1e-05dp0.2b32ntest1-Img+Meta2+logMAR+FHScore+6F+driving')
-for (model in mods) {
-  print (model)
-  cor_ = c() 
-  r2 = c() 
-
-  df_ = read.csv(paste0('C:/Users/duongdb/Documents/FH_OCT_08172021/Classify/',model,'/final_prediction.csv'))
-  cor_ = c(cor_,cor(df_$label, df_$average_score, method = c("spearman"))) # pearson spearman
-  lmmod = lm(df_$label ~ df_$average_score)
-  r2 = c(r2, summary(lmmod)$r.squared)
-  
-  print (summary(cor_))
-  print (summary(r2))
-  print (table(df_$label))
-  plot (df_$label,df_$average_score)
-  boxplot (df_$average_score~df_$label, xlab='True label', ylab='Average score', ylim=c(1,3))
-}
